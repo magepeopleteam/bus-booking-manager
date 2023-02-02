e1654ae9bbc3fac1135750253bd25c0e764f56d6
unknown <rabiul0420@gmail.com>
2023-02-02 12:27:12 +0600

    name convension fixed

--- inc/NextDateClass.php
+++ inc/NextDateClass.php
@@ -12,7 +12,7 @@ class NextDateClass extends CommonClass
    public function mage_next_date_suggestion($return, $single_bus, $target)
     {
         $date = $return ? $this->mage_bus_isset('r_date') : $this->mage_bus_isset('j_date');
-        $date = $this->wbtm_convert_date_to_php($date);
+        $date = $this->wbbm_convert_date_to_php($date);
         if ($date) {
             $tab_date = isset($_GET['tab_date']) ? $_GET['tab_date'] : mage_wp_date($this->mage_bus_isset('j_date'), 'Y-m-d');
             $tab_date_r = isset($_GET['tab_date_r']) ? $_GET['tab_date_r'] : mage_wp_date($this->mage_bus_isset('r_date'), 'Y-m-d');
@@ -26,14 +26,14 @@ class NextDateClass extends CommonClass
                         ?>
                         <li class="<?php echo $date == $next_date ? 'mage_active' : ''; ?>">
                             <a href="<?php echo $single_bus ? '' : get_site_url() . '/' . $target; ?>?bus_start_route=<?php echo strip_tags($_GET['bus_start_route']); ?>&bus_end_route=<?php echo strip_tags($_GET['bus_end_route']); ?>&j_date=<?php echo $return ? strip_tags($_GET['j_date']) : $next_date_text; ?>&r_date=<?php echo $return ? $next_date : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>&bus-r=<?php echo (isset($_GET['bus-r']) ? strip_tags($_GET['bus-r']) : ''); ?>&tab_date=<?php echo $tab_date; ?>&tab_date_r=<?php echo $tab_date_r; ?>" data-sroute='<?php echo strip_tags($_GET['bus_start_route']); ?>' data-eroute='<?php echo strip_tags($_GET['bus_end_route']); ?>' data-jdate='<?php echo $return ? strip_tags($_GET['j_date']) : $next_date; ?>' data-rdate='<?php echo $return ? $next_date : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>' class='wbtm_next_day_search'>
-                                <?php echo $this->get_wbtm_datetime($next_date, 'date-text') ?>
+                                <?php echo $this->get_wbbm_datetime($next_date, 'date-text') ?>
                                 <?php //echo mage_wp_date($next_date);
                                 ?>
                             </a>
                         </li>
                         <?php
                         $next_date = date('Y-m-d', strtotime($next_date . ' +1 day'));
-                        // $next_date_text = get_wbtm_datetime($next_date, 'date-text');
+
                         $next_date_text = $next_date;
                     }
                     ?>
@@ -48,7 +48,7 @@ class NextDateClass extends CommonClass
     function mage_next_date_suggestion_single($return, $single_bus, $target)
     {
         $j_date = $this->mage_bus_isset('j_date');
-        $j_date = $this->wbtm_convert_date_to_php($j_date);
+        $j_date = $this->wbbm_convert_date_to_php($j_date);
         $wbtm_bus_on_dates = get_post_meta(get_the_id(), 'wbtm_bus_on_date', true) ? maybe_unserialize(get_post_meta(get_the_id(), 'wbtm_bus_on_date', true)) : [];
         $wbtm_offday_schedules = get_post_meta(get_the_id(), 'wbtm_offday_schedule', true)?get_post_meta(get_the_id(), 'wbtm_offday_schedule', true):[];
         $weekly_offday = get_post_meta(get_the_id(), 'weekly_offday', true) ? get_post_meta(get_the_id(), 'weekly_offday', true):[];
@@ -63,13 +63,13 @@ class NextDateClass extends CommonClass
                     <?php
                     $wbtm_bus_on_dates_arr = explode(',', $wbtm_bus_on_dates);
                     foreach ($wbtm_bus_on_dates_arr as $i => $ondate) {
-                        if ($j_date <= $this->wbtm_convert_date_to_php($ondate)) {
-                            $ondate = ($i) ? $this->wbtm_convert_date_to_php($ondate) : $j_date;
+                        if ($j_date <= $this->wbbm_convert_date_to_php($ondate)) {
+                            $ondate = ($i) ? $this->wbbm_convert_date_to_php($ondate) : $j_date;
                             ?>
                             <li class="<?php echo $j_date == $ondate ? 'mage_active' : ''; ?>">
                                 <a href="<?php echo $single_bus ? '' : get_site_url() . '/' . $target; ?>?bus_start_route=<?php echo strip_tags($_GET['bus_start_route']); ?>&bus_end_route=<?php echo strip_tags($_GET['bus_end_route']); ?>&j_date=<?php echo $return ? strip_tags($_GET['j_date']) : $ondate; ?>&r_date=<?php echo $return ? $ondate : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>&bus-r=<?php echo (isset($_GET['bus-r']) ? strip_tags($_GET['bus-r']) : ''); ?>" data-sroute='<?php echo strip_tags($_GET['bus_start_route']); ?>' data-eroute='<?php echo strip_tags($_GET['bus_end_route']); ?>' data-jdate='<?php echo $return ? strip_tags($_GET['j_date']) : $next_date; ?>' data-rdate='<?php echo $return ? $next_date : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>' class='wbtm_next_day_search'>
                                     <?php
-                                    echo $this->get_wbtm_datetime($ondate, 'date-text')
+                                    echo $this->get_wbbm_datetime($ondate, 'date-text')
                                     ?>
                                 </a>
                             </li>
@@ -111,7 +111,7 @@ class NextDateClass extends CommonClass
                             ?>
                             <li class="<?php echo $j_date == $next_date ? 'mage_active' : ''; ?>">
                                 <a href="<?php echo $single_bus ? '' : get_site_url() . '/' . $target; ?>?bus_start_route=<?php echo strip_tags($_GET['bus_start_route']); ?>&bus_end_route=<?php echo strip_tags($_GET['bus_end_route']); ?>&j_date=<?php echo $return ? strip_tags($_GET['j_date']) : $next_date_text; ?>&r_date=<?php echo $return ? $next_date : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>&bus-r=<?php echo (isset($_GET['bus-r']) ? strip_tags($_GET['bus-r']) : ''); ?>" data-sroute='<?php echo strip_tags($_GET['bus_start_route']); ?>' data-eroute='<?php echo strip_tags($_GET['bus_end_route']); ?>' data-jdate='<?php echo $return ? strip_tags($_GET['j_date']) : $next_date; ?>' data-rdate='<?php echo $return ? $next_date : (isset($_GET['r_date']) ? strip_tags($_GET['r_date']) : ''); ?>' class='wbtm_next_day_search'>
-                                    <?php echo $this->get_wbtm_datetime($next_date, 'date-text') ?>
+                                    <?php echo $this->get_wbbm_datetime($next_date, 'date-text') ?>
                                 </a>
                             </li>
                             <?php
@@ -136,51 +136,7 @@ class NextDateClass extends CommonClass
         return isset($_GET[$parameter]) ? strip_tags($_GET[$parameter]) : false;
     }
 
-    public function wbtm_convert_date_to_php($date)
-    {
 
-        $date_format = get_option('date_format');
-        if ($date_format == 'Y-m-d' || $date_format == 'm/d/Y' || $date_format == 'm/d/Y') {
-            if ($date_format == 'd/m/Y') {
-                $date = str_replace('/', '-', $date);
-            }
-        }
-        return date('Y-m-d', strtotime($date));
-    }
-
-    function get_wbtm_datetime($date, $type)
-    {
-        $date_format = get_option('date_format');
-        $time_format = get_option('time_format');
-        $wpdatesettings = $date_format . '  ' . $time_format;
-        $timezone = wp_timezone_string();
-        $timestamp = strtotime($date . ' ' . $timezone);
-
-        if ($type == 'date') {
-            return wp_date($date_format, $timestamp);
-        }
-        if ($type == 'date-time') {
-            return wp_date($wpdatesettings, $timestamp);
-        }
-        if ($type == 'date-text') {
-
-            return wp_date($date_format, $timestamp);
-        }
-
-        if ($type == 'date-time-text') {
-            return wp_date($wpdatesettings, $timestamp, wp_timezone());
-        }
-        if ($type == 'time') {
-            return wp_date($time_format, $timestamp, wp_timezone());
-        }
-
-        if ($type == 'day') {
-            return wp_date('d', $timestamp);
-        }
-        if ($type == 'month') {
-            return wp_date('M', $timestamp);
-        }
-    }
 
 }
 
