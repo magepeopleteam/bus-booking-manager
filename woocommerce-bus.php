1c7d78ca560068322cb11aba5f3accdd5c95d928
shohan777 <shohan.st27@gmail.com>
2023-03-09 14:09:51 +0600

    Fix time format

--- woocommerce-bus.php
+++ woocommerce-bus.php
@@ -4,7 +4,7 @@
  * Plugin Name: Multipurpose Ticket Booking Manager (Bus/Train/Ferry/Boat/Shuttle)
  * Plugin URI: http://mage-people.com
  * Description: A Complete Ticket Booking System for WordPress & WooCommerce
- * Version: 4.0.7
+ * Version: 4.0.8
  * Author: MagePeople Team
  * Author URI: http://www.mage-people.com/
  * Text Domain: bus-booking-manager
