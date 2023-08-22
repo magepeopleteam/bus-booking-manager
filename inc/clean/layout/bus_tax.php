95ba9953d368ccded63905b41fb4063fcc2288f3
arifulislamnayem <arifulislamnayem@gmail.com>
2023-08-22 10:15:10 +0600

    Update bus_tax.php

--- inc/clean/layout/bus_tax.php
+++ inc/clean/layout/bus_tax.php
@@ -10,20 +10,22 @@
                 $post_id = $post->ID;
                 $tax_status = get_post_meta($post_id, '_tax_status', true);
                 $tax_class = get_post_meta($post_id, '_tax_class', true);
+                $MP_Global_Function = new MP_Global_Function();
+                $tax_lists = $MP_Global_Function->all_tax_list();
             ?>
             <select name="_tax_status" id="_tax_status">
                 <option value="taxable" <?php if($tax_status == 'taxable'){ echo 'selected'; } ?>>Taxable</option>
                 <option value="shipping" <?php if($tax_status == 'shipping'){ echo 'selected'; } ?>>Shipping only</option>
                 <option value="none" <?php if($tax_status == 'none'){ echo 'selected'; } ?>>None</option>
             </select>
-            </span>
         </div>
         <div class="sec">
             <label for="_tax_class" class="ra-item-label">Tax Class</label>
             <span>
             <select name="_tax_class" id="_tax_class">
-                <option value="reduced-rate" <?php if($tax_class == 'reduced-rate'){ echo 'selected'; } ?>>Reduced rate</option>
-                <option value="zero-rate" <?php if($tax_class == 'zero-rate'){ echo 'selected'; } ?>>Zero rate</option>
+                <?php foreach ($tax_lists as $key => $value) { ?>
+                    <option value="<?php echo $key; ?>" <?php if($tax_class == $key){ echo 'selected'; } ?>><?php echo $value; ?></option>
+                <?php } ?>
             </select>
             </span>
         </div>
